2d6d54869290ac2a87ee098f5cdd8c530ef98ad0
한승현 <vkflek1232@naver.com>
2024-02-08 06:43:04 +0900

    project init

---
+++ mynotion/src/main/java/com/mynotion/MynotionApplication.java
@@ -0,0 +1,13 @@
+package com.mynotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynotionApplication.class, args);
+	}
+
+}
